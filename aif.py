f9ca3b10f8992e8c6ef869ce5020c0a9b676a6ac
Anton Ermolin <anton@mannet.ru>
2024-06-20 10:48:08 +0300

    Update aif.py

--- aif.py
+++ aif.py
@@ -19,126 +19,119 @@ pio.templates.default = 'seaborn'
 # титульный текст приложения
 st.title('АиФ "Доброе сердце"')
 
-# создаем боковое меню
-with st.sidebar:
-    st.subheader("Выберите опцию") # заголовок меню
-    rfm_button = st.sidebar.button(label='RFM analysis') # кнопка РФМ
-    # img_file_buffer = st.sidebar.button(label='make selfie') # эта кнопка захватывает изображение с камеры компа/ноута
+uploaded_file = st.sidebar.file_uploader('Загрузи файл для анализа')
 
-# работа кнопки РФМ: делаем РФМ анализ и выводим на экран основные моменты
-if rfm_button:
-    uploaded_file = st.file_uploader('Загрузи файл для анализа')
-    if uploaded_file is not None:
-            orders = pd.read_csv(uploaded_file, sep=';', encoding='cp1251', usecols=[2, 3, 5, 17, 20, 21, 30])
-            orders.columns = ['order_datetime', 'channel_id', 'channel_name', 'order_aim', 'order_sum', 'order_status', 'user_id']
-            orders.order_datetime = pd.to_datetime(orders.order_datetime, dayfirst=True).dt.date
-            pays = orders[orders.order_status == 'Paid']
-            # выводим на экран количество счетов
-            st.write(f'Всего оплаченных счетов: {pays.shape[0]}')
-            st.write(f'Сумма оплаченных счетов: {pays.order_sum.sum():,} рублей')
-            st.write(f'Средний чек: {pays.order_sum.sum()/ pays.shape[0]:.2f} рублей')
-            min_date, max_date = pays.order_datetime.min(), pays.order_datetime.max()
+if uploaded_file is not None:
+        orders = pd.read_csv(uploaded_file, sep=';', encoding='cp1251', usecols=[2, 3, 5, 17, 20, 21, 30])
+        orders.columns = ['order_datetime', 'channel_id', 'channel_name', 'order_aim', 'order_sum', 'order_status', 'user_id']
+        orders.order_datetime = pd.to_datetime(orders.order_datetime, dayfirst=True).dt.date
+        pays = orders[orders.order_status == 'Paid']
+        # выводим на экран количество счетов
+        st.write(f'Всего оплаченных счетов: {pays.shape[0]}')
+        st.write(f'Сумма оплаченных счетов: {pays.order_sum.sum():,} рублей')
+        st.write(f'Средний чек: {pays.order_sum.sum()/ pays.shape[0]:.2f} рублей')
+        min_date, max_date = pays.order_datetime.min(), pays.order_datetime.max()
         
-            # бабахаем график
-            pays_line = pays.groupby('order_datetime')['order_sum'].sum().reset_index()
-            fig_sales = go.Figure()
-            fig_sales.add_trace(go.Scatter(x=pays_line.order_datetime, 
-                                           y=pays_line.order_sum, 
-                                           name='сумма донаций',
-                                           line=go.scatter.Line(color="lightslategrey")))
-            fig_sales.add_trace(go.Scatter(x=pays_line.order_datetime, 
-                                           y=pays_line.order_sum.rolling(15).mean(), 
-                                           name='скользящее среднее', 
-                                           line=go.scatter.Line(color="crimson",
-                                                                width=3)))
-            fig_sales.update_layout(title='Сумма донаций по дням, руб.', legend_orientation="h", legend=dict(x=.5, xanchor="center"))
-            st.plotly_chart(fig_sales)
+        # бабахаем график
+        pays_line = pays.groupby('order_datetime')['order_sum'].sum().reset_index()
+        fig_sales = go.Figure()
+        fig_sales.add_trace(go.Scatter(x=pays_line.order_datetime, 
+                                        y=pays_line.order_sum, 
+                                        name='сумма донаций',
+                                        line=go.scatter.Line(color="lightslategrey")))
+        fig_sales.add_trace(go.Scatter(x=pays_line.order_datetime, 
+                                        y=pays_line.order_sum.rolling(15).mean(), 
+                                        name='скользящее среднее', 
+                                        line=go.scatter.Line(color="crimson",
+                                                            width=3)))
+        fig_sales.update_layout(title='Сумма донаций по дням, руб.', legend_orientation="h", legend=dict(x=.5, xanchor="center"))
+        st.plotly_chart(fig_sales)
         
-            st.markdown('### RFM анализ')
-            # определяем период анализа
-            st.write(f"Период анализа: с {min_date} по {max_date}, всего - {(max_date - min_date) / pd.Timedelta('1d')} дней")
-            # определяем период жизни донаторов
-            pays['period'] = (
-            pays.groupby('user_id')['order_datetime']
-            .transform(lambda cell: int((cell.max() - cell.min()) / pd.Timedelta('1d')) + 1)
-            )
+        st.markdown('### RFM анализ')
+        # определяем период анализа
+        st.write(f"Период анализа: с {min_date} по {max_date}, всего - {(max_date - min_date) / pd.Timedelta('1d')} дней")
+        # определяем период жизни донаторов
+        pays['period'] = (
+        pays.groupby('user_id')['order_datetime']
+        .transform(lambda cell: int((cell.max() - cell.min()) / pd.Timedelta('1d')) + 1)
+        )
         
-            # определяем дату анализа (максимальная дата + 1 день) и выводим на экран
-            now = pays.order_datetime.max() + pd.Timedelta('1d')
-            st.write(f'Дата отсчета для RFM анализа: {now}')
+        # определяем дату анализа (максимальная дата + 1 день) и выводим на экран
+        now = pays.order_datetime.max() + pd.Timedelta('1d')
+        st.write(f'Дата отсчета для RFM анализа: {now}')
             
-            # определяем РФМ значения
-            pays['R_value'] = pays.order_datetime.apply(lambda cell: int(((now - cell) / pd.Timedelta('1d'))))
-            pays['M_value'] = pays.groupby('user_id')['order_sum'].transform('sum')
-            pays['F_value'] = pays.groupby('user_id')['user_id'].transform('count') / pays.period
+        # определяем РФМ значения
+        pays['R_value'] = pays.order_datetime.apply(lambda cell: int(((now - cell) / pd.Timedelta('1d'))))
+        pays['M_value'] = pays.groupby('user_id')['order_sum'].transform('sum')
+        pays['F_value'] = pays.groupby('user_id')['user_id'].transform('count') / pays.period
         
-            # создаем и печатаем боксплот для R
-            fig_R_value = plt.figure(figsize=(12, 4))
-            color_r = 'crimson'
-            (
-                plt.boxplot(x=pays.R_value, notch=True, vert=False,
-                        boxprops=dict(color=color_r),
-                        capprops=dict(color=color_r),
-                        whiskerprops=dict(color=color_r),
-                        flierprops=dict(color=color_r, markeredgecolor=color_r),
-                        medianprops=dict(color=color_r)
-                        ))
-            plt.title('Распределение значений R-value')
-            plt.ylabel('R_value')
-            st.pyplot(fig_R_value)
+        # создаем и печатаем боксплот для R
+        fig_R_value = plt.figure(figsize=(12, 4))
+        color_r = 'crimson'
+        (
+            plt.boxplot(x=pays.R_value, notch=True, vert=False,
+                    boxprops=dict(color=color_r),
+                    capprops=dict(color=color_r),
+                    whiskerprops=dict(color=color_r),
+                    flierprops=dict(color=color_r, markeredgecolor=color_r),
+                    medianprops=dict(color=color_r)
+                    ))
+        plt.title('Распределение значений R-value')
+        plt.ylabel('R_value')
+        st.pyplot(fig_R_value)
         
-            # бьем R на ранги
-            r_bins = [0, 225, 547, pays.R_value.max()]
-            r_labels = [3, 2, 1]
-            pays['R'] = pd.cut(pays.R_value, bins=r_bins, labels=r_labels)
-            st.write('Выбросы отсутствуют. Значения разделены на 3 ранга')
+        # бьем R на ранги
+        r_bins = [0, 225, 547, pays.R_value.max()]
+        r_labels = [3, 2, 1]
+        pays['R'] = pd.cut(pays.R_value, bins=r_bins, labels=r_labels)
+        st.write('Выбросы отсутствуют. Значения разделены на 3 ранга')
         
-            # создаем и выводим на экран боксплот для F   
-            fig_F_value = plt.figure(figsize=(12, 4))
-            (
-                plt.boxplot(x=pays.F_value, notch=True, vert=False,
-                        boxprops=dict(color=color_r),
-                        capprops=dict(color=color_r),
-                        whiskerprops=dict(color=color_r),
-                        flierprops=dict(color=color_r, markeredgecolor=color_r),
-                        medianprops=dict(color=color_r)
-                        ))
-            plt.title('Распределение значений F-value')
-            plt.ylabel('F_value')
-            st.pyplot(fig_F_value)
+        # создаем и выводим на экран боксплот для F   
+        fig_F_value = plt.figure(figsize=(12, 4))
+        (
+            plt.boxplot(x=pays.F_value, notch=True, vert=False,
+                    boxprops=dict(color=color_r),
+                    capprops=dict(color=color_r),
+                    whiskerprops=dict(color=color_r),
+                    flierprops=dict(color=color_r, markeredgecolor=color_r),
+                    medianprops=dict(color=color_r)
+                    ))
+        plt.title('Распределение значений F-value')
+        plt.ylabel('F_value')
+        st.pyplot(fig_F_value)
         
-            # бьем F на ранги
-            f_bins = [0, .027, .11, pays.F_value.max()]
-            f_labels = [1, 2, 3]
-            pays['F'] = pd.cut(pays.F_value, bins=f_bins, labels=f_labels)
-            st.write('Выбросы присутствуют. Значения очищены и разбиты на 3 ранга')
+        # бьем F на ранги
+        f_bins = [0, .027, .11, pays.F_value.max()]
+        f_labels = [1, 2, 3]
+        pays['F'] = pd.cut(pays.F_value, bins=f_bins, labels=f_labels)
+        st.write('Выбросы присутствуют. Значения очищены и разбиты на 3 ранга')
         
-            # создаем и выводим на экран боксплот для M
-            fig_M_value = plt.figure(figsize=(12, 4))
-            (
-                plt.boxplot(x=pays.M_value, notch=True, vert=False,
-                        boxprops=dict(color=color_r),
-                        capprops=dict(color=color_r),
-                        whiskerprops=dict(color=color_r),
-                        flierprops=dict(color=color_r, markeredgecolor=color_r),
-                        medianprops=dict(color=color_r)
-                        ))
-            plt.title('Распределение значений M-value')
-            plt.ylabel('M_value')
-            st.pyplot(fig_M_value)
+        # создаем и выводим на экран боксплот для M
+        fig_M_value = plt.figure(figsize=(12, 4))
+        (
+            plt.boxplot(x=pays.M_value, notch=True, vert=False,
+                    boxprops=dict(color=color_r),
+                    capprops=dict(color=color_r),
+                    whiskerprops=dict(color=color_r),
+                    flierprops=dict(color=color_r, markeredgecolor=color_r),
+                    medianprops=dict(color=color_r)
+                    ))
+        plt.title('Распределение значений M-value')
+        plt.ylabel('M_value')
+        st.pyplot(fig_M_value)
         
-            # бьем М на ранги
-            m_bins = [-0.1, 600, 2800, pays.M_value.max()]
-            m_labels = [1, 2, 3]
-            pays['M'] = pd.cut(pays.M_value, bins=m_bins, labels=m_labels)
-            st.write('Выбросы присутствуют. Значения очищены и разбиты на 3 ранга')
+        # бьем М на ранги
+        m_bins = [-0.1, 600, 2800, pays.M_value.max()]
+        m_labels = [1, 2, 3]
+        pays['M'] = pd.cut(pays.M_value, bins=m_bins, labels=m_labels)
+        st.write('Выбросы присутствуют. Значения очищены и разбиты на 3 ранга')
         
-            # Сцеплаем ранги в общий ранг RFM
-            pays['RFM'] = pays.R.astype('str') + pays.F.astype('str') + pays.M.astype('str')
+        # Сцеплаем ранги в общий ранг RFM
+        pays['RFM'] = pays.R.astype('str') + pays.F.astype('str') + pays.M.astype('str')
         
-            # st.markdown позволяет выводить текст с разметкой как в юпитере
-            st.markdown('**Описание сегментов**')
-            st.markdown('''
+        # st.markdown позволяет выводить текст с разметкой как в юпитере
+        st.markdown('**Описание сегментов**')
+        st.markdown('''
                         Итого получаем таблицу с идентификаторами пользователя и сегментом (просьба заказчика). Метрики расчитывались следующим образом: 1 - плохо, 2 - терпимо, 3 - хорошо   
         **R:** **1** - был давно (больше 546 дней), **2** - не заходил от 225 до 546 дней, **3** - был недавно (не более 225 дней с момента визита  
         **F:** **1** - заходит не часто (не более 1 раза за 38 дней), **2** - средняя активность(1 заход в течение 9 - 38 дней),  **3** - высокая активность (чаще 1 раза в 9 дней)   
@@ -153,38 +146,38 @@ if rfm_button:
         |**3** - был менее 225 дней назад|**3** - заходит чаще 0.11 раз в день|**3** - заплатил более 2800 рублей за все время|
         ''')
         
-            # готовим таблицу для RFM сегментов    
-            rfm_stats = pays.groupby('RFM').agg({'user_id':'nunique', 'order_sum': ['count', 'mean', 'sum']}).reset_index()
+        # готовим таблицу для RFM сегментов    
+        rfm_stats = pays.groupby('RFM').agg({'user_id':'nunique', 'order_sum': ['count', 'mean', 'sum']}).reset_index()
             
-            rfm_stats.columns = ['RFM сегмент', 'Человек в сегменте, чел.',
-                             'Количество пожертвований, ед.', 'Среднее пожертвование, руб.',
-                             'Сумма пожертвований, руб.']
-            rfm_stats['test'] = rfm_stats['Сумма пожертвований, руб.']
+        rfm_stats.columns = ['RFM сегмент', 'Человек в сегменте, чел.',
+                            'Количество пожертвований, ед.', 'Среднее пожертвование, руб.',
+                            'Сумма пожертвований, руб.']
+        rfm_stats['test'] = rfm_stats['Сумма пожертвований, руб.']
         
-            st.write('---')
+        st.write('---')
           
-            test_df = pays[['order_datetime', 'order_sum', 'user_id', 'RFM']]
-            test_df['period'] = pd.to_datetime(test_df.order_datetime).dt.to_period('M')
-            test_df = test_df.groupby(['RFM', 'period'])['order_sum'].sum().reset_index()
-            test_df = test_df.sort_values(by='period')
+        test_df = pays[['order_datetime', 'order_sum', 'user_id', 'RFM']]
+        test_df['period'] = pd.to_datetime(test_df.order_datetime).dt.to_period('M')
+        test_df = test_df.groupby(['RFM', 'period'])['order_sum'].sum().reset_index()
+        test_df = test_df.sort_values(by='period')
             
-            rfm_stats = rfm_stats.merge(test_df.groupby('RFM')['order_sum'].apply(list).reset_index(), left_on='RFM сегмент', right_on='RFM')
-            rfm_stats = rfm_stats.drop(columns=['RFM'])
-            # выводим на экран итоговую таблицу
-            st.data_editor(rfm_stats.set_index('RFM сегмент').style.format(thousands=',', decimal='.', precision=2).background_gradient(cmap='Blues'),
+        rfm_stats = rfm_stats.merge(test_df.groupby('RFM')['order_sum'].apply(list).reset_index(), left_on='RFM сегмент', right_on='RFM')
+        rfm_stats = rfm_stats.drop(columns=['RFM'])
+        # выводим на экран итоговую таблицу
+        st.data_editor(rfm_stats.set_index('RFM сегмент').style.format(thousands=',', decimal='.', precision=2).background_gradient(cmap='Blues'),
                            column_config={
-                "test": st.column_config.ProgressColumn(  # этот код делает правую колонку с прогресс баром
-                    "Сумма донаций",
-                    help="Общая сумма донаций",
-                    format="%f руб.",
-                    min_value=0,
-                    max_value=rfm_stats.test.max(),
+            "test": st.column_config.ProgressColumn(  # этот код делает правую колонку с прогресс баром
+                "Сумма донаций",
+                help="Общая сумма донаций",
+                format="%f руб.",
+                min_value=0,
+                max_value=rfm_stats.test.max(),
                 ),
-                "order_sum": st.column_config.BarChartColumn(
-                    "Платежи по месяцам",
-                    help="The sales volume in the last 6 months",
-                    y_min=0,
-                    y_max=500000,
+            "order_sum": st.column_config.BarChartColumn(
+                "Платежи по месяцам",
+                help="The sales volume in the last 6 months",
+                y_min=0,
+                y_max=500000,
                 ),
                 'RFM сегмент': st.column_config.TextColumn(width='small'), # этот код и ниже уменьшает ширину колонок
                 'Человек в сегменте, чел.': st.column_config.NumberColumn(width='small'), # почитаейте про stramlit column_config
